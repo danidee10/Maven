2394e13972cb67b968a088cfd2d7cebd30fe1a3d
danidee10 <osaetindaniel@gmail.com>
2015-08-25 21:32:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
 __author__ = 'danidee'
-
+import os
 import sys
-from itertools import zip_longest
-import graphviz as gv
+import time
 from paths import *
-from level import *
-from graph import *
+
 
 
 def main():
     app = QtGui.QApplication(sys.argv)
-
+    app.setWindowIcon(QtGui.QIcon('img/icon.png'))
+    if os.name == 'nt':
+        # This is needed to display the app icon on the taskbar on Windows 7,8 and 10
+        import ctypes
+        myappid = 'MavenIcon...version 1.0'
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+            
+    pixmap = QtGui.QPixmap('img/splash.png')
+    splash = QtGui.QSplashScreen(pixmap)
+    splash.show()
+    app.processEvents()
+    time.sleep(3)
     activity = criticalPath()
     activity.get_num_act()
     activity.show()
+    splash.finish(activity)
     sys.exit(app.exec_())
-
-    '''*********************All the calculations for the resource levelling class*******************'''
-
-    levelit = level()
-    levelit.level_est(all_objects)
-    all_objects = levelit.level_lst(all_objects)
-
-    #list comprehension to add all the values in the est and lst of the objects
-    result = [[sum(j) for j in zip_longest(*i, fillvalue=0)] for i in zip_longest(*[activity.level for activity in all_objects], fillvalue=[0])]
-
-    for i in result:
-        print(i)
-
-    total = [[sum(j)] for j in [i for i in result]]
-
-    print(total)
-
-    [print('The efficiency {0:.1f} percent'.format(b/a * 100)) for a, b in zip_longest(*total)]
 
 if __name__ == '__main__': main()
